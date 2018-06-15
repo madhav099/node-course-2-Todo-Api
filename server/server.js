eac7ec05d273c0fe2cb54018d1e318147f49ae61
Mayank Yadav <madhav0990@gmail.com>
2018-06-15 17:27:58 +0530

    Mongoose changes

--- server/server.js
+++ server/server.js
@@ -5,36 +5,24 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String        
     },
     completed: {
         type: Boolean
     },
-    completedAt: {
+    completedAt: {  
         type: Number
     }
 });
 
 var newTodo = new Todo({
     text: 'Cook Dinner',
-    completed: 'false',
-    completedAt: 5
+    completed: false,
+    completedAt: 6
 });
 
 newTodo.save().then((doc) => {
-    console.log('Saved Todo', doc);
-},(e) => {
-    console.log('Unable to save Todo');
-});
-
-var secondTodo = new Todo({
-    text: 'Cook Lunch',
-    completed: true,
-    completedAt: 2
-});
-
-secondTodo.save().then((doc) => {
-    console.log(JSON.stringify(doc, undefined, 2));
-},(e) => {
-    console.log('Unable to Save Data');
+    console.log("Saved Todo", doc);
+}, (e) => {
+    console.log("Unable to save todo");
 });
